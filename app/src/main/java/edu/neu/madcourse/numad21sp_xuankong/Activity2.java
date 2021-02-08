1a7490ca0b45d1408a2d4d0a9a7dc0991216f7b6
xkong93 <k.xuan29@gmail.com>
2021-02-08 14:07:19 -0800

    create new activity

---
+++ app/src/main/java/edu/neu/madcourse/numad21sp_xuankong/Activity2.java
@@ -0,0 +1,19 @@
+package edu.neu.madcourse.numad21sp_xuankong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+
+}
--- app/src/main/java/edu/neu/madcourse/numad21sp_xuankong/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21sp_xuankong/MainActivity.java
@@ -2,6 +2,7 @@ package edu.neu.madcourse.numad21sp_xuankong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -21,5 +22,10 @@ public class MainActivity extends AppCompatActivity {
         String email = "kong.xu@husky.neu.edu";
         String other = "Go Husky!";
         textView.setText(name + "\n" + email + "\n" + other);
+    }
+
+    public void OnGotoNewActivity(View view) {
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
     }
 }
