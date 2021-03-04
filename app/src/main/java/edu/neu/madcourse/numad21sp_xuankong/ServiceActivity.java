575988b5c94ece78103e1911ddaaad12befe4717
xkong93 <k.xuan29@gmail.com>
2021-03-04 00:01:42 -0800

    add service runnable and api call

--- app/src/main/java/edu/neu/madcourse/numad21sp_xuankong/ServiceActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21sp_xuankong/ServiceActivity.java
@@ -3,18 +3,89 @@ package edu.neu.madcourse.numad21sp_xuankong;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class ServiceActivity extends AppCompatActivity {
     Button btService;
-
+    private Handler handler = new Handler();
+    String requestUrl = "https://api.covidtracking.com/v1/us/current.json";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
         btService = findViewById(R.id.LocatorButton);
-
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
     }
 
+
+    public void runService(View view) {
+        serviceRunnable serviceRunnable = new serviceRunnable();
+        new Thread(serviceRunnable).start();
+    }
+
+    class serviceRunnable implements Runnable {
+        @Override
+        public void run() {
+            handler.post(() -> {
+                try {
+                    Log.d("ss","test");
+
+                    HttpURLConnection httpClient =
+                            (HttpURLConnection) new URL(requestUrl).openConnection();
+
+                    // optional default is GET
+                    httpClient.setRequestMethod("GET");
+
+                    //add request header
+                    httpClient.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+                    int responseCode = httpClient.getResponseCode();
+                    System.out.println("\nSending 'GET' request to URL : " + requestUrl);
+                    System.out.println("Response Code : " + responseCode);
+
+                    try (BufferedReader in = new BufferedReader(
+                            new InputStreamReader(httpClient.getInputStream()))) {
+
+                        StringBuilder response = new StringBuilder();
+                        String line;
+
+                        while ((line = in.readLine()) != null) {
+                            response.append(line);
+                        }
+                        Integer numberNewCases = getNumberNewCases(response.toString());
+                        System.out.println(numberNewCases);
+                    }
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        private Integer getNumberNewCases(String response) throws JSONException {
+            JSONArray jsonArray = new JSONArray(response);
+            JSONObject jsonObject = (JSONObject) jsonArray.get(0);
+            String newCases = jsonObject.getString("positiveIncrease");
+            return Integer.valueOf(newCases);
+        }
+    }
 
 }
